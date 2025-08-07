104c3ab8063fd3881f6d0d6e6b645b89cce7118e
jaodograu <126591871+jaodograu@users.noreply.github.com>
2025-08-07 09:52:14 -0300

    Add files via upload

---
+++ AULADE/Variaveis.java
@@ -0,0 +1,18 @@
+public class Variaveis{
+    public static void main(String[] args) {
+        // tipo nome = conteudo;
+
+        String meuNome = "Burro ";
+        meuNome += "dos Inferno";
+        int idade = 69;
+        double altura = 1.57;
+        char sexo = 'M'
+        boolean casado = false;
+
+        System.out.print("Me chamo "+meuNome);
+        System.out.print(" minha idade e "+idade);
+        System.out.print(" anos e minha altura e "+altura);
+        System.out.print(" sou do sexo "+sexo);
+
+    }
+}
